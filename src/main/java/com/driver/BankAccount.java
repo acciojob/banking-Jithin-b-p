aa458e0bc683051288eeaae6e0753c961816d9a5
unknown <jithinbp007@gmail.com>
2023-03-24 21:41:21 +0530

    bank

--- src/main/java/com/driver/BankAccount.java
+++ src/main/java/com/driver/BankAccount.java
@@ -1,21 +1,4 @@
 package com.driver;
-class MaxWithdrawLimitException extends Exception{
-
-    public MaxWithdrawLimitException(String str){
-        super(str);
-    }
-}
-class AccountNumberException extends Exception{
-    public AccountNumberException(String str){
-        super(str);
-    }
-}
-class InsufficientBalanceException extends Exception{
-    public InsufficientBalanceException(String str) {
-
-        super(str);
-    }
-}
 public class BankAccount {
 
     private String name;
@@ -57,20 +40,23 @@ public class BankAccount {
         //Each digit of an account number can lie between 0 and 9 (both inclusive)
         //Generate account number having given number of 'digits' such that the sum of digits is equal to 'sum'
         //If it is not possible, throw "Account Number can not be generated" exception
-        int tempDigits = digits;
-        int tempSum = 0;
-        while(tempDigits != 0){
-            tempSum += (tempDigits % 10);
-            tempDigits /= 10;
+        if(digits * 9 < sum){
+            throw new Exception("Account Number can not be generated");
         }
-        if(tempSum != sum){
+        StringBuilder acNo = new StringBuilder("");
+        int num = 9;
+        while(sum != 0){
 
-            throw new AccountNumberException("Account Number can not be generated");
+            if(num <= sum){
+                sum -= num;
+                acNo.append(num);
+            }else{
+                num = sum;
+            }
         }
-
-        return null;
+        while(acNo.length() < digits) acNo.append(0);
+        return acNo.toString();
     }
-
     public void deposit(double amount) {
 
         balance += amount;
@@ -80,7 +66,7 @@ public class BankAccount {
     public void withdraw(double amount) throws Exception {
         // Remember to throw "Insufficient Balance" exception, if the remaining amount would be less than minimum balance
         if(amount > balance){
-            throw new InsufficientBalanceException("Insufficient Balance");
+            throw new Exception("Insufficient Balance");
         }
         balance -= amount;
 
--- src/main/java/com/driver/SavingsAccount.java
+++ src/main/java/com/driver/SavingsAccount.java
@@ -31,34 +31,24 @@ public class SavingsAccount extends BankAccount{
         // Might throw the following errors:
         // 1. "Maximum Withdraw Limit Exceed" : If the amount exceeds maximum withdrawal limit
         // 2. "Insufficient Balance" : If the amount exceeds balance
-        if(amount > super.getBalance()){
+        if(amount > maxWithdrawalLimit){
 
-            throw new InsufficientBalanceException("Insufficient Balance");
-
-        }else if(amount > maxWithdrawalLimit){
-
-            throw new MaxWithdrawLimitException("Maximum Withdraw Limit Exceed");
+            throw new Exception("Maximum Withdraw Limit Exceed");
 
         }
-        super.setBalance(super.getBalance() - amount);
+        super.withdraw(amount);
 
     }
 
     public double getSimpleInterest(int years){
         // Return the final amount considering that bank gives simple interest on current amount
-        double amount = super.getBalance();
-        double simpleInterest = 0.0;
-        simpleInterest = amount * rate * years / 100;
-        return amount + simpleInterest;
+        return getBalance() * (1 + years * rate / 100);
     }
 
     public double getCompoundInterest(int times, int years){
         // Return the final amount considering that bank gives compound interest on current amount given times per year
-        double amount = super.getBalance();
-        double compoundInterest = 0.0;
-        compoundInterest = (amount * (Math.pow(1 +(rate / 100), times * years))) - amount;
+        return getBalance() * Math.pow((1 + rate / (100 * times)), years * times);
 
-        return amount + compoundInterest;
     }
 
 }
